7fc859af84569a4556cb55d411be8e3841433dda
mrkRiyad <dev.riyad@gmail.com>
2018-10-03 03:24:32 +0600

    Add Model

---
+++ AspNetWebApiWithKnockoutJs/AspNetWebApiWithKnockoutJs/ViewModel/CourseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetWebApiWithKnockoutJs.ViewModel
+{
+    public class CourseViewModel
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }  
+    }
+}
---
+++ AspNetWebApiWithKnockoutJs/AspNetWebApiWithKnockoutJs/ViewModel/StudentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using AspNetWebApiWithKnockoutJs.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspNetWebApiWithKnockoutJs.ViewModel
+{
+    public class StudentViewModel
+    {
+        public int StudentID { get; set; }
+        [StringLength(25)]
+        public string Name { get; set; }
+        [Range(24, 32)]
+        public int Age { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public int CourseID { get; set; }
+    }
+}
