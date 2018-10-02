f2b7d5974559487eecf80d9a84fe6bf1e95dde4b
mrkRiyad <dev.riyad@gmail.com>
2018-10-03 03:25:48 +0600

    Add Controllers

---
+++ AspNetWebApiWithKnockoutJs/AspNetWebApiWithKnockoutJs/Controllers/CourseController.cs
@@ -0,0 +1,40 @@
+﻿using AspNetWebApiWithKnockoutJs.Models;
+using AspNetWebApiWithKnockoutJs.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AspNetWebApiWithKnockoutJs.Controllers
+{
+    // api/course
+    public class CourseController : ApiController
+    {
+        StudentDbContext db = new StudentDbContext();
+
+
+        public IEnumerable<CourseViewModel> Get()
+        {
+            var model = db.Courses.Select(c => new CourseViewModel
+            {
+                CourseID = c.CourseID,
+                CourseName = c.CourseName
+            });
+            return model;
+        }
+
+
+        public CourseViewModel Get(int id)
+        {
+            var c = db.Courses.Find(id);
+            var model = new CourseViewModel
+            {
+                CourseID = c.CourseID,
+                CourseName = c.CourseName
+            };
+            return model;
+        }
+    }
+}
---
+++ AspNetWebApiWithKnockoutJs/AspNetWebApiWithKnockoutJs/Controllers/StudentController.cs
@@ -0,0 +1,91 @@
+﻿using AspNetWebApiWithKnockoutJs.Models;
+using AspNetWebApiWithKnockoutJs.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AspNetWebApiWithKnockoutJs.Controllers
+{
+    // api/student
+    public class StudentController : ApiController
+    {
+        StudentDbContext db = new StudentDbContext();
+
+        
+        public IEnumerable<StudentViewModel> Get()
+        {
+            var model = db.Students.Select(s => new StudentViewModel
+            {
+                StudentID = s.StudentID,
+                Name = s.Name,
+                Age = s.Age,
+                Email = s.Email,
+                ProfileImageUrl = s.ProfileImageUrl,
+                CourseID = s.fkCourseID
+            });
+            return model;
+        }
+
+        
+        public StudentViewModel Get(int id)
+        {
+            var s = db.Students.Find(id);
+            var model = new StudentViewModel
+            {
+                StudentID = s.StudentID,
+                Name = s.Name,
+                Age = s.Age,
+                Email = s.Email,
+                ProfileImageUrl = s.ProfileImageUrl,
+                CourseID = s.fkCourseID
+            };
+            return model;
+        }
+
+        
+        public void Post(StudentViewModel std)
+        {
+            if (std.StudentID == 0) 
+            {
+                Student student = new Student();    
+                student.Name = std.Name;
+                student.Age = std.Age;
+                student.Email = std.Email;
+                student.ProfileImageUrl = std.ProfileImageUrl;
+                student.fkCourseID = std.CourseID;
+                if (ModelState.IsValid) 
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                }
+            }
+            else
+            {
+                Student student = db.Students.Find(std.StudentID);
+                if (student != null)
+                {
+                    student.Name = std.Name;
+                    student.Age = std.Age;
+                    student.Email = std.Email;
+                    student.ProfileImageUrl = std.ProfileImageUrl;
+                    student.fkCourseID = std.CourseID;
+                }
+                db.SaveChanges();
+            }
+        }
+
+        
+        public void Delete(int id)
+        {
+            Student student = db.Students.Find(id);
+            if (student != null)
+            {
+                db.Students.Remove(student);
+                db.SaveChanges();
+            }
+        }
+    }
+}
