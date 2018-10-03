ada68241ff8f876e83cc2259ffd1530a2033d2d8
mrkRiyad <dev.riyad@gmail.com>
2018-10-03 11:52:41 +0600

    Add OWIN Startup class and use cookie authentication

---
+++ AspNetWebApiWithKnockoutJs/AspNetWebApiWithKnockoutJs/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AspNetWebApiWithKnockoutJs.Startup))]
+
+namespace AspNetWebApiWithKnockoutJs
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Home/Login")
+            });
+        }
+    }
+}
