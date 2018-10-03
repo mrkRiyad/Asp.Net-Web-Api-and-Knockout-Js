47ee5ed3d14527107f3006eea761fbf8076b4f02
mrkRiyad <dev.riyad@gmail.com>
2018-10-03 11:49:19 +0600

    Add Identity Model

---
+++ AspNetWebApiWithKnockoutJs/AspNetWebApiWithKnockoutJs/Models/Security.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetWebApiWithKnockoutJs.Models
+{
+    public class Security : IdentityDbContext<AppUser>
+    {
+        public Security() : base("KnockoutJsIdentity")
+        {
+
+        }
+    }
+
+    public class AppUser : IdentityUser
+    {
+
+    }
+}
